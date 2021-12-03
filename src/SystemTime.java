d897a6b04c291d3e3295776b54241b336c096ff3
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-03 13:55:15 +0700

    Add file

---
+++ src/SystemTime.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+//        System.out.println("Hello em");
+        Date now = new Date();
+
+        System.out.println("Today is "+now);
+    }
+}
